20aa78e1bec8cf0d44327c5a634d9fcab25250d5
James Gary <gmarchesi888@gmail.com>
2025-01-15 16:44:51 -0500

    Closer to working.

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 const texts = [
     `The artichoke is the finest of all vegetables.`,
     `A nun writing her name in marmalade on a soldier's leg.`,
+    'Light coursing from a swarming moon that careens in frozen ecstasy across the sky',
 ]
 async function generateEmbeddings(texts) {
     const embeddingsArray = []
@@ -19,8 +20,10 @@ async function generateEmbeddings(texts) {
             const response = await openai.embeddings.create({
                 model: 'text-embedding-3-large',
                 input: text,
+                encoding_format: 'float',
             })
-            embeddingsArray.push(response.data.embedding)
+            // console.log('Embeddings response:', response.data[0].embedding)
+            embeddingsArray.push(response.data[0].embedding)
         }
     } catch (error) {
         console.error('Error creating embeddings:', error)
@@ -29,11 +32,11 @@ async function generateEmbeddings(texts) {
 }
 
 async function createAndStoreEmbeddings(embeddings) {
-    console.log('embeddings:', embeddings)
+    // console.log('embeddings:', embeddings)
     const indexName = 'embeddings-index'
     const indexes = await pinecone.listIndexes()
-    console.log('-------------')
-    console.log('Pinecone Indexes:', indexes.indexes)
+    // console.log('-------------')
+    // console.log('Pinecone Indexes:', indexes.indexes)
 
     const doesIndexExist = (name) => {
         console.log('Checking existence of index..', indexes)
@@ -51,7 +54,7 @@ async function createAndStoreEmbeddings(embeddings) {
         await pinecone.createIndex({
             name: indexName,
             // dimension: embeddings[0].length, -- OpenAI is returning "undefined", which is breaking it at the moment
-            dimension: 8,
+            dimension: 3072,
             spec: { serverless: { cloud: 'aws', region: 'us-east-1' } },
         })
         console.log('Index created.')
@@ -59,15 +62,15 @@ async function createAndStoreEmbeddings(embeddings) {
 
     const index = await pinecone.Index(indexName)
     const items = [
-        // { id: 'item_1', values: embeddings[0] },
-        // { id: 'item_2', values: embeddings[1] },
-        { id: 'item_1', values: [0.2, 0.1, 0.3, 0.6, 0.3, 0.7, 0.3, 0.87] },
-        { id: 'item_2', values: [0.01, 0.3, 0.77, 0.65, 0.3, 0.43, 0.2, 0.3] },
+        { id: 'artichoke', values: embeddings[0] },
+        { id: 'marmalade', values: embeddings[1] },
+        // { id: 'item_1', values: [0.2, 0.1, 0.3, 0.6, 0.3, 0.7, 0.3, 0.87] },
+        // { id: 'item_2', values: [0.01, 0.3, 0.77, 0.65, 0.3, 0.43, 0.2, 0.3] },
     ]
-    console.log('Items from Pinecone function:', items)
+    // console.log('Items from Pinecone function:', items)
     await index.upsert(items)
 
-    console.log('Embeddings stored in Pinecone!')
+    // console.log('Embeddings stored in Pinecone!')
 }
 
 async function queryPinecone(queryEmbedding) {
@@ -86,9 +89,9 @@ async function queryPinecone(queryEmbedding) {
 
 generateEmbeddings(texts)
     .then((embeddings) => {
-        console.log('Generated Embeddings From OpenAI:', embeddings)
+        // console.log('Generated Embeddings From OpenAI:', embeddings)
         createAndStoreEmbeddings(embeddings)
     })
-    .then(() => {
-        queryPinecone('blah_blah_blah') // embedding values don't matter because everything is hardcoded right now.
+    .then((embeddings) => {
+        queryPinecone(embeddings) // embedding values don't matter because everything is hardcoded right now.
     })
